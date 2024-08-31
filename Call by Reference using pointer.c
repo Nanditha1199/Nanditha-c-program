2c0154ab2115f5698542a3bb9c85dbf0f1bcff8f
Nanditha1199 <dnanditha1199@gmail.com>
2024-08-31 13:41:01 +0530

    Create Call by Reference using pointer.c

---
+++ Call by Reference using pointer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int *x,int *y)
+{
+int temp = *x;
+*x=*y;
+*y=temp;
+}
+int main()
+{
+int a=10,b=20;
+printf("before swapping a=%d,b=%d\n",a,b);
+swap(&a,&b);
+printf("after swapping a=%d,b=%d\n",a,b);
+return 0;
+}
